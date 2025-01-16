0db6ff27bc5167bb7f1ae662678df57d38301ac7
jeffemart <jefferson.github@gmail.com>
2025-01-15 21:05:55 -0300

    correcting CI.yml to implement basic test

---
+++ app/gin/testdata/example.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	return router
+}
+
+func main() {
+	router := setupRouter()
+	router.Run(":8080")
+}
